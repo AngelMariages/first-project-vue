fd3b2d1cbd0a97f47e48d6c6537b4c337cd22dc2
AngelMariages <angel.mariages@gmail.com>
2020-05-14 17:06:52 +0200

    try sw config

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,13 @@ module.exports = {
 	publicPath: process.env.NODE_ENV === 'production'
 		? '/first-project-vue/'
 		: '/',
+	pwa: {
+		name: 'Àngel first project in Vue',
+		workboxOptions: {
+			runtimeCaching: [{
+				urlPattern: new RegExp('https://cdn.jsdelivr.net/npm/bulma'),
+				handler: 'StaleWhileRevalidate',
+			}],
+		},
+	},
 };
